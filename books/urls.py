e9adf4b03a3f93ed781ef2f85e9ebc0933a45a62
Mikayel Harutyunyan <mikayelharutyunyan@Mikayels-MacBook-Pro-2.local>
2026-04-17 19:57:02 +0400

    update

--- books/urls.py
+++ books/urls.py
@@ -2,10 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.home_page, name='home_page'),
     path('register/', views.client_register, name='register'),
     path('login/', views.client_login, name='login'),
     path('logout/', views.client_logout, name='logout'),
-    path('home/', views.home_page, name='home_page'),
     path('contact/', views.contact_page, name='contact_page'),
     path('author/<int:author_id>/', views.author_page, name='author_page'),
     path('author/<int:author_id>/<str:book_title>/', views.book_page, name='book_page'),
